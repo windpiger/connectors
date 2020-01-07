527c1359368ff243bdd0599bda532588698e98c8
windpiger <songjun@outlook.com>
2020-01-07 09:43:09 +0800

    to #24243352 remove set hive.input.format

--- hive/src/test/scala/io/delta/hive/test/HiveTest.scala
+++ hive/src/test/scala/io/delta/hive/test/HiveTest.scala
@@ -38,7 +38,7 @@ trait HiveTest extends FunSuite with BeforeAndAfterAll {
     conf.set("hive.metastore.schema.verification", "false")
     conf.set("datanucleus.schema.autoCreateAll", "true")
     conf.set("hive.fetch.task.conversion", "none")
-    conf.set("hive.input.format", "org.apache.hadoop.hive.ql.io.HiveInputFormat")
+//    conf.set("hive.input.format", "org.apache.hadoop.hive.ql.io.HiveInputFormat")
     conf.set(
       "javax.jdo.option.ConnectionURL",
       s"jdbc:derby:memory:;databaseName=${metastoreDir.getCanonicalPath};create=true")
